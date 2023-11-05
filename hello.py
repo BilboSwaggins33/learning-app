9f3747a9230ab9859bc84255f641e52fabe9e546
Aaron Zhang <aarondzhang33@gmail.com>
2023-11-05 04:01:06 -0800

    predashboard

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ import speech_recognition as sr
 # Load the video
 from werkzeug.utils import secure_filename
 import os
-
+from rake_nltk import Rake
 
 UPLOAD_FOLDER = './audiofiles'
 
@@ -25,8 +25,6 @@ def home():
 @app.route('/get_transcript', methods=['POST'])
 def get_transcript():
     youtube_link = request.form['youtube_link']
-    percentage = int(request.form['percentage'])
-    percentage = 3 - ((percentage/100) + 1);
     video_id = youtube_link.split('=')[1]
 
 
@@ -35,25 +33,27 @@ def get_transcript():
         transcript_text = ' '.join([entry['text'] for entry in transcript])
     except Exception as e:
         transcript_text = f"Error: {str(e)}"
-        new_summary = get_summary(transcript_text)
+        #new_summary = get_summary(transcript_text)
         sum = summarize_text(transcript_text)
-        return render_template('transcript.html', transcript=sum)
+        return render_template('transcript.html', transcript=sum, original_transcript=transcript_text)
 
     sum = summarize_text(transcript_text)
-    new_summary = get_summary(transcript_text, percentage)
+    #new_summary = get_summary(transcript_text, percentage)
     return render_template('transcript.html', transcript=sum, original_transcript=transcript_text)
 
 
-@app.route('/common_words', methods=['POST'])
+@app.route('/key-concepts', methods=['POST'])
 def get_common_words():
+    r = Rake()
     transcript_text = request.form['transcript']
-    words = transcript_text.lower().split()
-    common_words = Counter(words).most_common(20)  # Get the 10 most common words
+    # words = transcript_text.lower().split()
+    # common_words = Counter(words).most_common(20)  # Get the 10 most common words
+    r.extract_keywords_from_text(transcript_text)    
+    keywords = r.get_ranked_phrases()[0:10]
+
+    return render_template('common_words.html', common_words=keywords)
 
 
-    return render_template('common_words.html', common_words=common_words)
-if __name__ == '__main__':
-    app.run()
 
 @app.route('/transcript_file', methods=['POST'])
 def transcript_file():
@@ -76,8 +76,15 @@ def transcript_file():
     # Print the text
     print("\nThe resultant text from video is: \n")
     print(text)
-    return render_template('transcript.html', transcript=text, original_transcript=text)
+    sum = summarize_text(text)
 
+    return render_template('transcript.html', transcript=sum, original_transcript=text)
+
+
+
+
+if __name__ == '__main__':
+    app.run()
 # @app.route("/hello")
 # def hello():
 #     return "you are now on hello"

8b7b7637bd3b12e6b116e2a5a20a03f08e49ace9
Aaron Zhang <aarondzhang33@gmail.com>
2023-11-04 18:36:33 -0700

    file-upload

--- hello.py
+++ hello.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request
 from youtube_transcript_api import YouTubeTranscriptApi
 from collections import Counter
 
+import moviepy.editor as mp
+import speech_recognition as sr
+import os    
+os.environ["FFMPEG_BINARY"] = "/usr/local/bin/ffmpeg"
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,15 +17,36 @@ def home():
 def get_transcript():
     youtube_link = request.form['youtube_link']
     video_id = youtube_link.split('=')[1]
-
+    print(type(video_id))
     try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        transcript = YouTubeTranscriptApi.get_transcript(str(video_id))
         transcript_text = ' '.join([entry['text'] for entry in transcript])
     except Exception as e:
-        transcript_text = f"Error: {str(e)}"
+        transcript_text = "Error:" + str(e)
         return render_template('transcript.html', transcript=transcript_text)
 
     return render_template('transcript.html', transcript=transcript_text)
+
+@app.route('/transcript_file', methods=['POST'])
+def transcribe_file():
+    file = request.files['file']
+    video = mp.VideoFileClip(file)
+    # Extract the audio from the video
+    audio_file = video.audio
+    audio_file.write_audiofile("geeksforgeeks.wav")
+    # Initialize recognizer
+    r = sr.Recognizer()
+    # Load the audio file
+    with sr.AudioFile("geeksforgeeks.wav") as source:
+      data = r.record(source)
+    # Convert speech to text
+    text = r.recognize_google(data)
+    # Print the text
+    print("\nThe resultant text from video is: \n")
+    print(text)
+    return;
+
+    
 
 @app.route('/common_words', methods=['POST'])
 def get_common_words():
